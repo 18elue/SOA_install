a541bf60eca6c1c8e6cd198b09cbda4cfc814865
lu.ye <yelue666@gmail.com>
2015-01-13 14:14:02 +0800

    <SOA-INSTALL-PREPARE> change the way of getting ip

--- DOMAIN_CREATE_TEMPLATE/domain_create/set_osb_domain.sh
+++ DOMAIN_CREATE_TEMPLATE/domain_create/set_osb_domain.sh
@@ -10,7 +10,7 @@ DOMAIN_HOME=$DOMAIN_DIR/$DOMAIN_NAME
 #check if domain alread exists
 #[[ -e $DOMAIN_HOME ]] && echo "domain $DOMAIN_NAME already exists, exit now" && exit
 
-ip=$(hostname -i)
+ip=$(ifconfig|grep "inet addr:"|grep -v '127.0.0.1'|awk '{print $2}'|awk -F: '{print $2}')
 # create domain
 if [[ $ADMIN_SERVER_ADDRESS = $ip ]] && [[ $DOMAIN_TYPE = OSB ]]; then
     bash $WLST_PATH create_osb_domain.py
--- DOMAIN_CREATE_TEMPLATE/domain_create/set_soa_domain.sh
+++ DOMAIN_CREATE_TEMPLATE/domain_create/set_soa_domain.sh
@@ -10,7 +10,7 @@ DOMAIN_HOME=$DOMAIN_DIR/$DOMAIN_NAME
 #check if domain alread exists
 #[[ -e $DOMAIN_HOME ]] && echo "domain $DOMAIN_NAME already exists, exit now" && exit
 
-ip=$(hostname -i)
+ip=$(ifconfig|grep "inet addr:"|grep -v '127.0.0.1'|awk '{print $2}'|awk -F: '{print $2}')
 # create domain
 if [[ $ADMIN_SERVER_ADDRESS = $ip ]] && [[ $DOMAIN_TYPE = SOA ]]; then
     bash $WLST_PATH create_soa_domain.py
